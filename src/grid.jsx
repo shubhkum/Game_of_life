1fab309fd86d296b9ca1d315cd4f9079aae33ecc
shubhIsSmart <shubham.kumar@flairminds.com>
2024-07-18 18:04:23 +0530

    Added the instructions and rules to play the game

--- src/grid.jsx
+++ src/grid.jsx
@@ -107,10 +107,18 @@ export const Grid = () => {
         })
     }
    
-
     return (
         <div className={styles.gridContainer}>
             <div className={styles.gridWrapper}>
+                <div className={styles.instructions}>
+                    <h2 style={{textDecoration:'underline', textAlign:'center'}}>Instructions</h2>
+                    <div className={styles.texts}>
+                        <p>1. Draw an initial pattern on the grid at the center</p>
+                        <p>2. Click Start to allow multiplication of cells according to Conway's law</p>
+                        <p>3. Click Stop at any point in time to stop the multiplication</p>
+                        <p>4. Click Reset to set an initial pattern</p>
+                    </div>
+                </div>
                 <div className={styles.grids}>
                     {
                         mat?.map((row,rowInd) => {
@@ -135,6 +143,16 @@ export const Grid = () => {
                         </div>
                     </div>                    
                 </div>
+                <div className={styles.rules}>
+                    <h2 style={{textDecoration:'underline', textAlign:'center'}}>Rules of cell multiplication</h2>
+                    <div className={styles.texts}>
+                        <p>1. A dead cell will come alive if exactly 3 neighbours are living</p>
+                        <p>2. A living cell will stay alive if 2 or 3 neighbours are living</p>
+                        <p>3. Cells with less than 2 living neighbours will die of underpopulation</p>
+                        <p>4. Cells with more than 4 living neighbours will die of overpopulation</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
